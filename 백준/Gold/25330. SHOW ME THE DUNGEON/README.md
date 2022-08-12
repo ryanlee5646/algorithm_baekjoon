e852942a2e71a5f8911d0d0aacef80aa41868523
Gyujin Lee <45954499+ryanlee5646@users.noreply.github.com>
2022-08-12 09:32:55 +0900

    [Gold V] Title: SHOW ME THE DUNGEON, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/25330. SHOW ME THE DUNGEON/README.md
@@ -0,0 +1,12 @@
+# [Gold V] SHOW ME THE DUNGEON - 25330 
+
+[문제 링크](https://www.acmicpc.net/problem/25330) 
+
+### 성능 요약
+
+메모리: 30840 KB, 시간: 80 ms
+
+### 분류
+
+백트래킹(backtracking), 브루트포스 알고리즘(bruteforcing)
+
---
+++ 백준/Gold/25330. SHOW ME THE DUNGEON/SHOW ME THE DUNGEON.py
@@ -0,0 +1,30 @@
+# SHOW ME THE DUNGEON
+from sys import stdin
+input = stdin.readline
+
+
+def DFS(depth, life, saved_people, path):
+    global result
+    if life < 0:
+        return
+    if life >= 0:
+        result = max(result, saved_people)
+
+    for i in range(monster_num):
+        if not visited[i]:
+            visited[i] = 1
+            DFS(i, life - (sum(monsters[j] for j in path) +
+                monsters[i]), saved_people + people[i], path + [i])
+            visited[i] = 0
+
+
+monster_num, life = map(int, input().split())
+monsters = list(map(int, input().split()))
+people = list(map(int, input().split()))
+visited = [0] * monster_num
+
+result = 0
+
+DFS(0, life, 0, [])
+
+print(result)
