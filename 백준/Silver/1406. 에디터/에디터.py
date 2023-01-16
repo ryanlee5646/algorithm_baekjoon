635887e227eb3f88858febc38437f4f053b9b4d4
Gyujin Lee <45954499+ryanlee5646@users.noreply.github.com>
2023-01-16 23:26:55 +0900

    [Silver II] Title: 에디터, Time: 308 ms, Memory: 40904 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -0,0 +1,28 @@
+from sys import stdin
+input = stdin.readline
+
+words = list(input().strip())
+N = int(input())
+
+right = []
+
+for i in range(N):
+
+  command = input().rstrip().replace(" ", "")
+  
+  if command[0] == "L":
+    if words:
+      right.append(words.pop())
+      
+  elif command[0] == "D":
+    if right:
+      words.append(right.pop())
+      
+  elif command[0] == "B":
+    if words:
+      words.pop()
+      
+  elif command[0] == "P":
+      words.append(command[1])
+
+print(''.join(words + list(reversed(right))))
